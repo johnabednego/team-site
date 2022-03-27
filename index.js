9e320f11e0e7e441756f5f639badda14918d6f2d
johnabednego <jajilima@st.ug.edu.gh>
2022-03-27 01:51:26 +0000

    sun

---
+++ index.js
@@ -0,0 +1,17 @@
+// // Create the observer
+// const observer = new IntersectionObserver(entries => {
+//     // We will fill in the callback later...
+//     entries.forEach(entry => {
+//         const square = entry.target.querySelector('.board');
+//         if (entry.isIntersecting) {
+//           // It's visible. Add the animation class here!
+//           entry.target.classList.add('square-animation');
+//           return;
+//         }
+//         square.classList.remove('square-animation');
+//       });
+//   });
+  
+//   // Tell the observer which elements to track
+//   observer.observe(document.querySelector('.info'));
+
